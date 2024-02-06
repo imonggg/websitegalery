5c8e421cc50d1b0c8bc24415d25311fcfabf3cb9
Elisapurnamasari <Elisapurnamasari08@gmail.com>
2024-02-06 09:10:07 +0700

    first commit

---
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::create('gallery',function (Blueprint $table) {
+            $table->timertamps();
+            $table->string('name');
+            $table->taxt('alamat');
+            $table->text('deskripsi');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personal_access_tokens');
+    }
+};
